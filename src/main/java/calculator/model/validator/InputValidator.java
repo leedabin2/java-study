279facbeef04213b2e2392d7f0aa70d960aa66da
leedabin2 <12193152@inha.edu>
2024-10-31 21:40:35 +0900

    fix: 입력값과 구분자 커스텀 예외처리 추가

--- src/main/java/calculator/model/validator/InputValidator.java
+++ src/main/java/calculator/model/validator/InputValidator.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class InputValidator {
 
     public String[] validInput(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (input == null || input.trim().isEmpty()  || input.matches("//.\\\\n?")) {
             return new String[] {"0"};
         }
         String[] parseNumbers = parseInput(input);
@@ -26,19 +26,29 @@ public class InputValidator {
             return parseCustom(input);
         } else if (input.matches(".*[:|,].*")){
             return input.split("[:|,]");
-        } else {
+        } else if (input.matches("\\d+")) {
+            return new String[]{input};
+        }else {
             throw new IllegalArgumentException("유효하지 않은 입력 형식입니다.");
         }
     }
 
     public String[] parseCustom(String input) {
         int splitIndex = input.indexOf("\\n");
-        if (splitIndex == -1) {
+        if (splitIndex == -1 || splitIndex == input.length() - 2) {
             throw new IllegalArgumentException("잘못된 형식의 입력입니다. 구분자 형식이 올바르지 않습니다.");
         }
 
         String delimiter = input.substring(2, splitIndex);
         String numbersPart = input.substring(splitIndex + 2);
+
+        if (delimiter.isEmpty()|| delimiter.matches("\\d+")) {
+            throw new IllegalArgumentException("구분자는 숫자가 아닌 문자여야 합니다.");
+        }
+        if (numbersPart.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 형식의 구분자입니다.");
+        }
+
         return numbersPart.split(Pattern.quote(delimiter));
     }
 
